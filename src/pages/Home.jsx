06b5ffb60b0ba533db8b4207d34b6d807d2665dc
Leonardo Veque <leonardo5veque@gmail.com>
2023-08-18 10:53:42 -0300

    Coneccao feita

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,41 @@ import axios from "axios";
 function Home() {
   const [receitas, setReceitas] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [ing, setIng] = useState([]);
 
   useEffect(() => {
     async function load() {
-      const response = await api.get("search.php");
+      const response = await api.get("search.php?s=Cake");
+
+      const data = response.data;
+
+      console.log(data);
+      setReceitas(data.meals);
+      setLoading(false);
     }
+    load();
   }, []);
+
+  const pegarIng = () => (
+    <div>
+      {receitas.map((item) => (
+        <div className="receitas" key={item.idMeal}>
+          <h2>{item.strMeal}</h2>
+          <img src={item.strMealThumb} alt={item.strMeal} />
+          <p>{item.strInstructions}</p>
+        </div>
+      ))}
+    </div>
+  );
+
+  if (loading) {
+    return (
+      <div>
+        <h2>Carregando</h2>
+      </div>
+    );
+  }
+  return <div>{pegarIng()}</div>;
 }
 
 export default Home;
--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //base URL: https:localhost/4000
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000/search.php?s=Cake'
+    baseURL: 'http://localhost:4000'
 });
 
 export default api;
